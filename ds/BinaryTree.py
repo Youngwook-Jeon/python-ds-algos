4a3f858b270acbc068ccebbc554683f88a5c3b78
Young wook Jeon <ela87@naver.com>
2021-06-21 14:14:53 +0900

    commit

--- ds/BinaryTree.py
+++ ds/BinaryTree.py
@@ -153,3 +153,67 @@ def insert_node_BT(root_node, new_node):
             else:
                 root.value.right_child = new_node
                 return "Successfully Inserted"
+
+def get_deepest_node(root_node):
+    if not root_node:
+        return
+    else:
+        custom_queue = Queue()
+        custom_queue.enqueue(root_node)
+        while not (custom_queue.is_empty()):
+            root = custom_queue.dequeue()
+            if (root.value.left_child is not None):
+                custom_queue.enqueue(root.value.left_child)
+            if (root.value.right_child is not None):
+                custom_queue.enqueue(root.value.right_child)
+        deepest_node = root.value
+        return deepest_node
+
+def delete_deepest_node(root_node, d_node):
+    if not root_node:
+        return
+    else:
+        custom_queue = Queue()
+        custom_queue.enqueue(root_node)
+        while not (custom_queue.is_empty()):
+            root = custom_queue.dequeue()
+            if root.value is d_node:
+                root.value = None
+                return
+            if root.value.right_child:
+                if root.value.right_child is d_node:
+                    root.value.right_child = None
+                    return
+                else:
+                    custom_queue.enqueue(root.value.right_child)
+            if root.value.left_child:
+                if root.value.left_child is d_node:
+                    root.value.left_child = None
+                    return
+                else:
+                    custom_queue.enqueue(root.value.left_child)
+
+def delete_node_BT(root_node, node):
+    if not root_node:
+        return "The BT does not exist."
+    else:
+        custom_queue = Queue()
+        custom_queue.enqueue(root_node)
+        while not (custom_queue.is_empty()):
+            root = custom_queue.dequeue()
+            if root.value.data == node:
+                d_node = get_deepest_node(root_node)
+                root.value.data = d_node.data
+                delete_deepest_node(root_node, d_node)
+                return "The node has been successfully deleted."
+            if (root.value.left_child is not None):
+                custom_queue.enqueue(root.value.left_child)
+            if (root.value.right_child is not None):
+                custom_queue.enqueue(root.value.right_child)
+        return "Failed to delete."
+
+def delete_BT(root_node):
+    root_node.data = None
+    root_node.left_child = None
+    root_node.right_child = None
+    return "The BT has been successfully deleted."
