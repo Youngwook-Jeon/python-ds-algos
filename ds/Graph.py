e284469ccbfdc40605a68dd5675f5074b2c6964c
Young wook Jeon <ela87@naver.com>
2021-08-11 00:50:18 +0900

    commit

--- ds/Graph.py
+++ ds/Graph.py
@@ -17,9 +17,22 @@ class Graph:
                 if (adjacent_vertex not in visited):
                     visited.append(adjacent_vertex)
                     queue.append(adjacent_vertex)
+    
+    def dfs(self, vertex):
+        visited = [vertex]
+        stack = [vertex]
+        while stack:
+            popped_vertex = stack.pop()
+            print(popped_vertex)
+            for adjacent_vertex in self.gdict[popped_vertex]:
+                if adjacent_vertex not in visited:
+                    visited.append(adjacent_vertex)
+                    stack.append(adjacent_vertex)
 
 if __name__ == "__main__":
     custom_dict = { "a": ["b", "c"], "b": ["a", "d", "e"], "c": ["a", "e"], "d": ["b", "e"], "e": ["d", "c"] }
     graph = Graph(custom_dict)
     print(graph.gdict)
     graph.bfs("a")
+    print()
+    graph.dfs("a")
